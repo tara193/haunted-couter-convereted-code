74b3930d47b3d36f345f23a76a4b58305385b670
tara193 <60931969+tara193@users.noreply.github.com>
2021-02-15 12:27:33 +0530

    initial commit

--- src/HauntedCounter.jsx
+++ src/HauntedCounter.jsx
@@ -1,63 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export class HauntedCounter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: props.startFrom ?? 0
-    };
-  }
+export const HauntedCounter = (props) => {
+  const [counter, setCounter] = useState(props.startFrom ?? 0);
 
-  add = (amount) => () => {
-    this.setState(({ count }) => ({ count: count + amount }));
+  const add = (amount) => () => {
+    setCounter((count) => count + amount);
   };
 
-  poltergeist = () => {
+  const poltergeist = () => {
     console.log("Boooooh!");
-    this.setState(({ count }) => ({
-      count: count + Math.round(Math.random() * 2) - 1
-    }));
+    setCounter((count) => count + Math.round(Math.random() * 2) - 1);
   };
 
-  componentDidUpdate() {
-    if (Math.abs(this.state.count) < 25) {
-      setTimeout(this.poltergeist, 500 + Math.random() * 2500);
+  useEffect(() => {
+    if (Math.abs(counter) < 25) {
+      setTimeout(poltergeist, 500 + Math.random() * 2500);
     }
-  }
+  }, [counter]);
 
-  render() {
-    return (
-      <div
+  return (
+    <div
+      style={{
+        background: Math.abs(counter) > 5 ? "#E49273" : "#A8D0DB"
+      }}
+    >
+      <span style={{ padding: "8px" }}>{counter}</span>
+      <button
         style={{
-          background: Math.abs(this.state.count) > 5 ? "#E49273" : "#A8D0DB"
+          background: Math.abs(counter) > 5 ? "#DE7954" : "#7EB9C9"
         }}
+        onClick={add(1)}
       >
-        <span style={{ padding: "8px" }}>{this.state.count}</span>
-        <button
-          style={{
-            background: Math.abs(this.state.count) > 5 ? "#DE7954" : "#7EB9C9"
-          }}
-          onClick={this.add(1)}
-        >
-          Add
-        </button>
-        <button
-          style={{
-            background: Math.abs(this.state.count) > 5 ? "#DE7954" : "#7EB9C9"
-          }}
-          onClick={this.add(-1)}
-        >
-          Subtract
-        </button>
-        <button
-          style={{
-            background: Math.abs(this.state.count) > 5 ? "#DE7954" : "#7EB9C9"
-          }}
-          onClick={this.add(-this.state.count)}
-        >
-          Reset
-        </button>
-      </div>
-    );
-  }
-}
+        Add
+      </button>
+      <button
+        style={{
+          background: Math.abs(counter) > 5 ? "#DE7954" : "#7EB9C9"
+        }}
+        onClick={add(-1)}
+      >
+        Subtract
+      </button>
+      <button
+        style={{
+          background: Math.abs(counter) > 5 ? "#DE7954" : "#7EB9C9"
+        }}
+        onClick={add(-counter)}
+      >
+        Reset
+      </button>
+    </div>
+  );
+};
